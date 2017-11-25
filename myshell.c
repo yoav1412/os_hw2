b109b866c62e9296df37143cedcd62925b63d317
yoav <yoav1412@gmail.com>
2017-11-25 21:11:26 +0200

    added pipe feature

--- myshell.c
+++ myshell.c
@@ -27,17 +27,18 @@ int process_arglist(int count,char** arglist){
     int status;
 
     bool isBackgroundProcess = false;
-    if (strcmp(arglist[count - 1], "&") == 0) { // as argist[count] is the NULL termination.
+    if (strcmp(arglist[count - 1], "&") == 0) { // as arglist[count] is the NULL termination.
         isBackgroundProcess = true;
         arglist[count-1] = NULL; // in order not to pass "&" as an argument to execvp.
     }
 
     int pipeArgIndex;
+    int fds[2];
     bool isPipe = (pipeArgIndex = checkForPipe(arglist)) != -1;
     if (isPipe) {
         arglist[pipeArgIndex] = NULL;
+        pipe(fds);
     }
-
 
     for (int i=0; i < 1 + isPipe; i++){
         if ((child_pid = fork()) < 0){
@@ -47,20 +48,32 @@ int process_arglist(int count,char** arglist){
 
         if (child_pid == 0){
             // Child code:
-            signal(SIGINT, child_handler);
-            printf("In the child process\n"); // TODO: rm
+
+            signal(SIGINT, child_handler); //TODO: change to sigaction
+            //printf("In the child process\n"); // TODO: rm
+
+            if (isPipe){
+                // First process (i=0) will rplace stdout (1) with pipe and second (i=1) will replace stdin (0):
+                close(1-i);
+                dup(fds[1-i]);
+            }
+
             if (execvp(arglist[0 + i*(pipeArgIndex+1)],arglist + i*(pipeArgIndex +1)) == -1){
                 printf("execvp failed: %s\n", strerror(errno));
 
             }
-            printf("After execvp\n"); // TODO: rm
+            break; //Child does not continue loop.
         }
-        else {
-            signal(SIGINT, parent_handler);
-            while (!isBackgroundProcess && wait(&status) != child_pid){} // Wait for the child to finish.
-            printf("In fater. finished waiting for son.\n"); // TODO: rm
-        }
+        else{
+            signal(SIGINT, parent_handler); //TODO: change to sigaction
+            }
     }
+
+    if (child_pid != 0){
+        while (!isBackgroundProcess && wait(&status) != -1){} // Wait for child / children
+        printf("In fater. finished waiting for son(s).\n"); // TODO: rm
+    }
+
 
 
     //TODO: need to restore father process SIGINT handler here. (not default??..)
